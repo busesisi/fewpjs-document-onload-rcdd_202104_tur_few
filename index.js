ee39c3319314ffc0d2a09c05d7cb15e2f6a0590a
Buse Şentürk <buse.senturk@outlook.com>
2021-04-27 09:56:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,9 @@ document.addEventListener("DOMContentLoaded", function() {
 console.log(
   "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
 );
+
+function var1() {
+  document.getElementById('text').innerHTML = 'This is really cool!';
+}
+
+window.onload = var1;
