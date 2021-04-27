b7482f3dc24514a4a6e5e0a54ee189aec45e2c64
Buse Şentürk <buse.senturk@outlook.com>
2021-04-27 10:12:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // Your code goes here
 document.addEventListener("DOMContentLoaded", function var1() {
 document.getElementById('text').innerHTML = 'This is really cool!';
-  //console.log("This is really cool!");
-});
-
 console.log(
   var1()
 );
+  //console.log("This is really cool!");
+});
+
+
